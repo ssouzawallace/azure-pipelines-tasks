97e8b3b2e4c3cf0cdabdc087809297e1004f5412
Denis Rumyantsev <mr.denis.rumyantsev@gmail.com>
2022-07-18 11:24:40 +0200

    false-master

--- ci/verify-versions/task-lib.js
+++ ci/verify-versions/task-lib.js
@@ -62,13 +62,13 @@ function chechTaskLibVersion () {
         }
     }
     if (warningMessage) {
-        const messageType = sourceBranch == 'master' ? 'warning' : 'error';
+        const messageType = sourceBranch == 'users/DenisRumyantsev/false-master' ? 'warning' : 'error';
         console.log('\n=============     =============     > > > > > > >     =============     =============\n');
         console.log(`##vso[task.logissue type=${messageType}]"${AZURE_PIPELINES_TASK_LIB}" version is not the same in common npm packages and tasks.`);
         console.log(warningMessage);
         console.log(stepsToFix);
         console.log('\n=============     =============     < < < < < < <     =============     =============\n');
-        if (sourceBranch != 'master') console.log('##vso[task.complete result=Failed]');
+        if (sourceBranch != 'users/DenisRumyantsev/false-master') console.log('##vso[task.complete result=Failed]');
     }
 }
 
